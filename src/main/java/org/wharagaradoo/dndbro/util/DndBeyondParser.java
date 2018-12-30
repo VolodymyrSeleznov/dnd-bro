ed62280fce86863f088290f8558934f0330d1e67
Vladimir <vladimir_seleznov@gmail.com>
2018-12-30 13:08:22 +0200

    remove commented code

--- src/main/java/org/wharagaradoo/dndbro/util/DndBeyondParser.java
+++ src/main/java/org/wharagaradoo/dndbro/util/DndBeyondParser.java
@@ -14,24 +14,6 @@ import java.io.IOException;
 public class DndBeyondParser {
 
   public static void main(String[] args) throws IOException {
-    //    Document document = Jsoup.parse(new
-    // URL("https://www.dndbeyond.com/characters/7204016/JDn8T0"), 30000);
-
-    //    Connection.Response response =
-    // Jsoup.connect("https://www.dndbeyond.com/characters/7204016")
-    //            .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML,
-    // like Gecko) Chrome/32.0.1667.0 Safari/537.36")
-    //            .execute();
-    //
-    //    Document doc = Jsoup.connect("https://www.dndbeyond.com/characters/7204016/json")
-    //            .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML,
-    // like Gecko) Chrome/32.0.1667.0 Safari/537.36")
-    //            .header("Accept-Language", "en-US")
-    //            .header("Accept-Encoding", "gzip,deflate,sdch")
-    //            .cookies(response.cookies())
-    //            .get();
-    //    System.out.println();
-
     String json =
         Jsoup.connect("https://www.dndbeyond.com/character/7204016/json")
             .ignoreContentType(true)
